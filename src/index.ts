bc7a8e065a00a47e9ec08c5f2eebdc44990c9a46
Quan Truong <qmtruong92@gmail.com>
2023-07-25 14:00:44 -0700

    additional config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Colors {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const colors: Colors = {
+  red: 10,
+  blue: 5,
+  green: 3,
+};
+
+console.log(colors);
+
+export default colors;
