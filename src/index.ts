f10fb64de05242b84ba6b0c98dbac5db728c3be9
Quan Truong <qmtruong92@gmail.com>
2023-07-25 21:50:03 -0700

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-interface Colors {
-  red: number;
-  blue: number;
-  green: number;
-}
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-const colors: Colors = {
-  red: 10,
-  blue: 5,
-  green: 3,
-};
-
-console.log(colors);
-
-export default colors;
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
