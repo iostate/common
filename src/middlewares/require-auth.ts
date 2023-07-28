48499c5bccc5db44d77374e0066146ac1d0d603c
Quan Truong <qmtruong92@gmail.com>
2023-07-27 19:15:52 -0700

    updates

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { NotAuthorizedError } from '../errors/not-authorized-error';
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.currentUser);
   if (!req.currentUser) {
     throw new NotAuthorizedError();
   }
