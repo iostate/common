ed52843607b73f85916e9f719c979493af90a3e0
Quan Truong <qmtruong92@gmail.com>
2023-11-20 11:35:00 -0700

    updates

--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -4,6 +4,7 @@ export interface TicketUpdatedEvent {
   subject: Subjects.TicketUpdated;
   data: {
     id: string;
+    orderId: string;
     version: number;
     title: string;
     price: number;
